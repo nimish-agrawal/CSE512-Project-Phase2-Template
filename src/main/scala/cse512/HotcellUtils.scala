2eb6892c27d81dc0125a6d7d5f44532c442fd96b
Nimish Agrawal <nimish.agrawal21@gmail.com>
2020-04-17 20:52:59 -0700

    Update HotcellUtils.scala

--- src/main/scala/cse512/HotcellUtils.scala
+++ src/main/scala/cse512/HotcellUtils.scala
@@ -48,4 +48,62 @@ object HotcellUtils {
   }
 
   // YOU NEED TO CHANGE THIS PART
+  def calculateStandardDeviation(sumOfSquares: val, numCells: val, avg: val) : Double =
+  {
+    return scala.math.sqrt(((sumOfSquares.first().getDouble(0).toDouble / numCells.toDouble) - (avg.toDouble * avg.toDouble))).toDouble;
+  }
+
+  def calculateNumNeighbours(x: Int, y: Int, z: Int, minX: Int, maxX: Int, minY: Int, maxY: Int, minZ: Int, maxZ: Int): Int =
+  {
+    
+    // corner  
+    if ((x == minX || x == maxX) && (y == minY || y == maxY) && (z == minZ || z == maxZ)){
+      return 7;
+    }
+
+    // centre
+    if ((x != minX && x != maxX) && (y != minY && y != maxY) && (z != minZ && z != maxZ)){
+      return 26;
+    }
+
+    // edge
+    if (((x == minX || x == maxX) && (y == minY || y == maxY))){
+        return 11;
+    }
+    if (((x == minX || x == maxX) && (z == minZ || z == maxZ))){
+        return 11;
+    }
+    if (((y == minY || y == maxY) && (z == minZ || z == maxZ))){
+        return 11;
+    }
+     
+    // face
+    return 17;
+  }
+
+  // checks if two cells are neighbours
+  def isNeighbour(x1: Int, x2: Int, x3: Int, y1: Int, y2: Int, y3: Int, minX: Int, minY: Int, minZ: Int, maxX: Int, maxY: Int, maxZ: Int) : Boolean = 
+  {
+    if (x2 > maxX || x2 < minX || y2 < minY || y2 > maxY || z2 > maxZ || z2 < minZ){
+      return false;
+      } 
+    //else if ((x1 - 1 == x2 || x1 == x2 || x1 + 1 == x2) && (y1 - 1 == y2 || y1 == y2 || y2 == y1 + 1) && (z1 == z2 || z2 == z1 - 1 || z2 == z1 + 1)) 
+    else if((x1-x2).abs <= 1 && (y1-y2).abs <= 1 && (z1-z2).abs <= 1){
+      return true;
+      }
+    else{
+      return false;
+    }
+  }
+
+
+  def calculateGScore(numNeighnours: Int, sumHotCells: Int, numCells: Int, x: Int, y: Int, z: Int, mean: Double, standardDeviation: Double): Double =
+  {
+
+    val sumWijXj = sumHotCells.toDouble
+    val sumWij = numNeighnours.toDouble
+    val root = math.sqrt((((numCells.toDouble * sumWij) - (sumWij * sumWij)) / (numCells.toDouble - 1.0).toDouble).toDouble).toDouble
+
+    return (sumWijXj - (mean*sumWij)) / (standardDeviation*root)
+  }
 }
